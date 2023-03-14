43312f5ae896e659d3efe91c36ec5373a15dd2f8
TucaoBad00 <arthurbadotti@hotmail.com>
2023-03-14 09:45:30 -0300

    0.2

---
+++ Assets/Scripts/MenuButtonsManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class MenuButtonsManager : MonoBehaviour
+{
+    public List<GameObject> buttons;
+
+
+    private void showButtons()
+    {
+
+
+    }
+
+    IEnumerator ShowButtonsDeley()
+    {
+        
+    }
+}
